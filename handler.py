dcf247a360675f166cd0c51185fe9d04e818ec68
Thomas Ched <tom.chedru@gmail.com>
2024-11-14 09:57:22 +0000

    project completed!

--- handler.py
+++ handler.py
@@ -8,6 +8,9 @@ import uuid
 
 s3 = boto3.client('s3')
 size = int(os.environ['THUMBNAIL_SIZE'])
+dbtable = str(os.environ['DYNAMODB_TABLE'])
+dynamodb = boto3.resource(
+    'dynamodb', region_name=str(os.environ['REGION_NAME']))
 
 #Edited function name in yaml
 def s3_thumbnail_generator(event, context):
@@ -54,9 +57,10 @@ def upload_to_s3(bucket, key, image, img_size):
     # it from
     image.save(out_thumbnail, 'PNG')
     out_thumbnail.seek(0)
-
+    #WARNING: might need to comment out ACL line if breaking code.
+    #Solved by manually editing ACL permissions + public access read on the s3 bucket via aws console
     response = s3.put_object(
-        #ACL='public-read',
+        ACL='public-read',
         Body=out_thumbnail,
         Bucket=bucket,
         ContentType='image/png',
@@ -65,6 +69,87 @@ def upload_to_s3(bucket, key, image, img_size):
     print(response)
 
     url = '{}/{}/{}'.format(s3.meta.endpoint_url, bucket, key)
+    s3_save_thumbnail_url_to_dynamo(url_path=url, img_size=img_size)
 
     return url
 
+def s3_save_thumbnail_url_to_dynamo(url_path, img_size):
+    toint = float(img_size*0.53)/1000
+    table = dynamodb.Table(dbtable)
+    response = table.put_item(
+        Item={
+            'id': str(uuid.uuid4()),
+            'url': str(url_path),
+            'approxReducedSize': str(toint) + str(' KB'),
+            'createdAt': str(datetime.now()),
+            'updatedAt': str(datetime.now())
+        }
+    )
+
+# get all image urls from the bucked and show in a json format
+    return {
+        'statusCode': 200,
+        'headers': {'Content-Type': 'application/json'},
+        'body': json.dumps(response)
+    }
+
+def s3_get_item(event, context):
+
+    table = dynamodb.Table(dbtable)
+    response = table.get_item(Key={
+        'id': event['pathParameters']['id']
+    })
+
+    item = response['Item']
+
+    return {
+        'statusCode': 200,
+        'headers': {'Content-Type': 'application/json',
+                    'Access-Control-Allow-Origin': '*'},
+        'body': json.dumps(item),
+        'isBase64Encoded': False,
+    }
+
+
+def s3_delete_item(event, context):
+    item_id = event['pathParameters']['id']
+
+    # Set the default error response
+    response = {
+        "statusCode": 500,
+        "body": f"An error occured while deleting post {item_id}"
+    }
+    table = dynamodb.Table(dbtable)
+    response = table.delete_item(Key={
+        'id': item_id
+    })
+    all_good_response = {
+        "deleted": True,
+        "itemDeletedId": item_id
+    }
+
+   # If deletion is successful for post
+    if response['ResponseMetadata']['HTTPStatusCode'] == 200:
+        response = {
+            "statusCode": 200,
+            'headers': {'Content-Type': 'application/json',
+                        'Access-Control-Allow-Origin': '*'},
+            'body': json.dumps(all_good_response),
+        }
+    return response
+
+def s3_get_thumbnail_urls(event, context):
+    # get all image urls from the db and show in a json format
+    table = dynamodb.Table(dbtable)
+    response = table.scan()
+    data = response['Items']
+    # paginate through the results in a loop
+    while 'LastEvaluatedKey' in response:
+        response = table.scan(ExclusiveStartKey=response['LastEvaluatedKey'])
+        data.extend(response['Items'])
+
+    return {
+        'statusCode': 200,
+        'headers': {'Content-Type': 'application/json'},
+        'body': json.dumps(data)
+    }
